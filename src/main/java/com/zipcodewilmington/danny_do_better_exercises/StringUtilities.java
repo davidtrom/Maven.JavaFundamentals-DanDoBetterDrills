457244afe6fa1c56737863e1272ed5f6306fd2a2
David Trombello <david@MacBook-Pro-8.localdomain>
2019-10-24 16:06:19 -0400

    all test classes pass

--- src/main/java/com/zipcodewilmington/danny_do_better_exercises/StringUtilities.java
+++ src/main/java/com/zipcodewilmington/danny_do_better_exercises/StringUtilities.java
@@ -58,39 +58,57 @@ public class StringUtilities {
      * @param comparableValue the value to be compared against
      * @return the equivalence of two strings, `inputValue` and `comparableValue`
      */
-    public static Boolean compareTwoStrings(String inputValue, String comparableValue){
-        return null;
+    public static Boolean compareTwoStrings(String inputValue, String comparableValue)
+    {
+        Boolean expected = inputValue.equals((comparableValue));
+        return expected;
     }
 
     /**
      * @param inputValue the value input from user
      * @return the middle character of `inputValue`
      */
-    public static Character getMiddleCharacter(String inputValue){
-        return null;
+    public static Character getMiddleCharacter(String inputValue)
+    {
+        int midLength = inputValue.length() / 2;
+        return inputValue.charAt(midLength);
     }
 
     /**
      * @param spaceDelimitedString a string, representative of a sentence, containing spaces
      * @return the first sequence of characters
      */
-    public static String getFirstWord(String spaceDelimitedString){
-        return null;
+    public static String getFirstWord(String spaceDelimitedString)
+    {
+        String[] splitString = spaceDelimitedString.split(" ");
+        return splitString[0].toString();
     }
 
     /**
      * @param spaceDelimitedString a string delimited by spaces
      * @return the second word of a string delimited by spaces.
      */
-    public static String getSecondWord(String spaceDelimitedString){
-        return null;
+    public static String getSecondWord(String spaceDelimitedString)
+    {
+        String[] splitString = spaceDelimitedString.split(" ");
+        return splitString[1].toString();
+
     }
 
     /**
      * @param stringToReverse
      * @return an identical string with characters in reverse order.
      */
-    public static String reverse(String stringToReverse){
-        return null;
+    public static String reverse(String stringToReverse)
+    {
+        //String[] result = new String
+
+        StringBuilder reversed = new StringBuilder();
+        String[] chars = stringToReverse.split("");
+        for (int i = chars.length-1; i >=0; i--)
+        {
+            reversed.append(stringToReverse.charAt(i));
+        }
+        return reversed.toString();
     }
 }
